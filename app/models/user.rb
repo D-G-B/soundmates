ca738d7de359525564e923e2ff1104fc4cc8f40a
Joleana Shurley <joleanashurley@gmail.com>
2021-03-02 12:15:16 +0100

    add remaining chat models

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,12 @@ class User < ApplicationRecord
   has_many :user_skills
   has_many :skills, through: :user_skills
 
+  has_many :user_chats
+  has_many :chatrooms, through: :user_chats
+
+  has_many :messages
+
+
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
 end
---
+++ app/models/user_chat.rb
@@ -0,0 +1,4 @@
+class UserChat < ApplicationRecord
+  belongs_to :user
+  belongs_to :chatroom
+end
