c8f0d47cb6fbec895fb32aaf721daf8aae85d431
Joleana Shurley <joleanashurley@gmail.com>
2021-03-02 12:21:33 +0100

    complete model connections

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,7 +11,7 @@ class User < ApplicationRecord
   has_many :chatrooms, through: :user_chats
 
   has_many :messages
-
+  has_many :shares
 
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
