a0807e27eca53810a7e5df6c8c513bde320c5991
Joleana Shurley <joleanashurley@gmail.com>
2021-03-02 19:56:17 +0100

    chatrooms controller and actions WIP

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,2 +1,26 @@
 class ChatroomsController < ApplicationController
+
+  def index
+    @chatrooms = Chatroom.all
+    @chatrooms = @chatrooms.where(user: current_user, status: true)
+  end
+
+  def show
+    @chatrooms = Chatroom.all
+    @chatrooms = @chatrooms.where(user: current_user, status: true)
+    @chatroom = Chatroom.find(params[:id])
+    @user = current_user
+    @message = Message.new
+    authorize @chatroom
+  end
+
+  def create
+    @chatroom = Chatroom.new
+  end
+
+  private
+
+  def approve
+
+  end
 end
