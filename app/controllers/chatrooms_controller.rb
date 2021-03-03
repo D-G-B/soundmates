604b6ae1083d9ab14b749bbc9247bd12d8d82d9e
Daniel Brandt <daniel.gordon.brandt@gmail.com>
2021-03-03 14:12:24 +0100

    progress

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,17 +1,20 @@
 class ChatroomsController < ApplicationController
 
   def index
+    @user = current_user
     @chatrooms = Chatroom.all
-    @chatrooms = @chatrooms.where(user: current_user, status: true)
+    @chatrooms = policy_scope(Chatroom).order(created_at: :desc).where(user_id: @user)
+    # @chatrooms = @chatrooms.where(user: @user, status: true)
   end
 
   def show
+    @chatroom = Chatroom.find(params[:id])
+    @user = current_user
+    @chatrooms = policy_scope(Chatroom).order(created_at: :desc).where(user_id: @user)
+    @message = Message.new
+    authorize @chatroom
     # @chatrooms = Chatroom.all
     # @chatrooms = @chatrooms.where(user: current_user, status: true)
-    @chatroom = Chatroom.find(params[:id])
-    # @user = current_user
-    # @message = Message.new
-    authorize @chatroom
   end
 
   def create
--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   resources :users, :only => [:show]
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :chatrooms, only: [:show] do
-    resources :messages, only: [:create]
+  resources :chatrooms, only: [ :index, :show] do
+    resources :messages, only: [:show, :create]
   end
 end
