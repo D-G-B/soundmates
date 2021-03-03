73d3260df06829e6f44a98bcd73d29f265b4c1cc
Daniel Brandt <daniel.gordon.brandt@gmail.com>
2021-03-03 15:14:09 +0100

    more progress

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -2,15 +2,15 @@ class ChatroomsController < ApplicationController
 
   def index
     @user = current_user
-    @chatrooms = Chatroom.all
-    @chatrooms = policy_scope(Chatroom).order(created_at: :desc).where(user_id: @user)
+    # @userchats = UserChat.all
+    @userchats = policy_scope(UserChat).order(created_at: :desc).where(user_id: @user)
+    # @userchats = UserChat.all.select { |userchat| userchat.user_id == @user  }
     # @chatrooms = @chatrooms.where(user: @user, status: true)
   end
 
   def show
     @chatroom = Chatroom.find(params[:id])
-    @user = current_user
-    @chatrooms = policy_scope(Chatroom).order(created_at: :desc).where(user_id: @user)
+    @messages = @chatroom.messages
     @message = Message.new
     authorize @chatroom
     # @chatrooms = Chatroom.all
--- db/seeds.rb
+++ db/seeds.rb
@@ -113,6 +113,13 @@ puts "creating chatroom(s)"
 puts "3 chatrooms have been created nil,true,flase"
 puts "...finished"
 
+puts "creating user_chat"
+
+  UserChat.create!(
+    user_id:daniel.id,
+    chatroom_id: truechat.id
+  )
+
 puts "createing messages"
 
   Message.create!(
