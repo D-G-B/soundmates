0b16d8c6b13243b813724a59e4fccac0a90fc72c
Daniel Brandt <daniel.gordon.brandt@gmail.com>
2021-03-03 11:22:04 +0100

    comment out unused lines

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -6,11 +6,11 @@ class ChatroomsController < ApplicationController
   end
 
   def show
-    @chatrooms = Chatroom.all
-    @chatrooms = @chatrooms.where(user: current_user, status: true)
+    # @chatrooms = Chatroom.all
+    # @chatrooms = @chatrooms.where(user: current_user, status: true)
     @chatroom = Chatroom.find(params[:id])
-    @user = current_user
-    @message = Message.new
+    # @user = current_user
+    # @message = Message.new
     authorize @chatroom
   end
 
