407faada1ab36503676b6cb683f213a1b1db0e11
Kevin Agoh <kevinagoh@hotmail.fr>
2021-03-03 16:48:36 +0100

    index page structure done

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,4 +5,27 @@ class UsersController < ApplicationController
     @users = policy_scope(User).limit(3)
   end
 
+
+  #Collections by genre, methods
+
+  def jazz
+    @users = policy_scope(User).where(genre: "Jazz")
+  end
+
+  def hiphop
+    @users = policy_scope(User).where(genre: "Rock")
+  end
+
+  def rock
+    @users = policy_scope(User).where(genre: "Hip-Hop")
+  end
+
+  def electronic
+    @users = policy_scope(User).where(genre: "Electronic")
+  end
+
+  def funk
+    @users = policy_scope(User).where(genre: "Funk/Soul")
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,9 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  resources :users, only: [ :index ]
+  resources :users, only: [ :index ] do
+    collection do
+      get :rock, :hiphop, :jazz, :electronic, :funk
+    end
+  end
 end
