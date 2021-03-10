8d32760202d9131bcf714c159e06b5cfd43520c3
Herve Bonnaffoux <bonnaffouxherve@gmail.com>
2021-03-10 16:19:13 +0100

    change css homepage and search bar

--- app/javascript/components/navbar.js
+++ app/javascript/components/navbar.js
@@ -6,9 +6,28 @@ const initUpdateNavbarOnScroll = () => {
         navbar.classList.add('navbar-lewagon-grey');
       } else {
         navbar.classList.remove('navbar-lewagon-grey');
+
       }
     });
   }
 }
 
 export { initUpdateNavbarOnScroll };
+
+const initUpdateSearchBar = () => {
+  const searchbar = document.querySelector(".form-inline");
+  if (searchbar) {
+    window.addEventListener('scroll', () => {
+      if (window.scrollY >= 20) {
+        searchbar.classList.remove('search-bar-landing-page');
+      }
+      if (window.scrollY < 20) {
+        searchbar.classList.add('search-bar-landing-page');
+      }
+    });
+  }
+}
+
+export { initUpdateSearchBar };
+
+
